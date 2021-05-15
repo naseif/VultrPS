bded1898a58da8725a38634aa414cc48252c5547
Stefan Hoffmann <stho32github@gmail.com>
2021-05-15 11:44:41 +0200

    added autocomplete for operating system

--- VultrPS/Instances/New-VULTRInstance.ps1
+++ VultrPS/Instances/New-VULTRInstance.ps1
@@ -7,12 +7,26 @@ function New-VULTRInstance {
     param (
         [string]$Region = "ams",
         [string]$Plan = "vc2-4c-8gb",
-        [string]$OsId = "387",
+    	[ArgumentCompleter({
+	        param( $CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters )
+            $temp = $WordToComplete.Replace('"', '')
+            (Get-VULTROperatingSystem) | 
+                Where-Object Name -like "*$temp*" | 
+                Foreach-Object {
+                    $os = $_.Name
+                    '"' + $os + '"'
+                }
+        })]
+        [ValidateScript({((Get-VULTROperatingSystem) | Where-Object Name -eq $_)})]
+        [string]$OperatingSystem = "Ubuntu 20.04 x64",
         [Switch]$WaitForResponsiveness
     )
 
     
     process {
+        # translate the name of the OS into the id
+        $OsId = (Get-VULTROperatingSystem | Where-Object Name -eq $OperatingSystem).id
+
         $data = @{
             region = $Region
             plan   = $Plan
